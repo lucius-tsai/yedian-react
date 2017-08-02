da1d745f545e6b38bfb4aa38c7c1bfe42b1e3531
townmi <towne766@126.com>
2017-08-02 16:31:04 +0800

    update api

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,7 +2,7 @@ let userApiDomain = null; // user infromation core service
 let userCoreApiDomain = null; // user buiness information core service
 let commontCoreApiDomain = null; // feedback system
 let venuesCoreApiDomain = null; // venues sysytem
-let cmsCoreApiDomain = null; // venues sysytem
+let cmsCoreApiDomain = null; // CMS sysytem
 
 if (process.env.NODE_ENV === "development") {
 	userApiDomain = "//user.dev.ye-dian.com";
@@ -18,40 +18,49 @@ if (process.env.NODE_ENV === "development") {
 	venuesCoreApiDomain = "//venuescore.staging.ye-dian.com";
 	cmsCoreApiDomain = "//cmscore.staging.ye-dian.com";
 } else {
-	userApiDomain = "//user.staging.ye-dian.com";
-	userCoreApiDomain = "//userCore.staging.ye-dian.com";
-	commontCoreApiDomain = "//feedback.staging.ye-dian.com";
-	venuesCoreApiDomain = "//venuescore.staging.ye-dian.com";
-	cmsCoreApiDomain = "//cmscore.staging.ye-dian.com";
+	userApiDomain = "//user.prod-v1.ye-dian.com";
+	userCoreApiDomain = "//userCore.prod-v1.ye-dian.com";
+	commontCoreApiDomain = "//feedback.prod-v1.ye-dian.com";
+	venuesCoreApiDomain = "//venuescore.prod-v1.ye-dian.com";
+	cmsCoreApiDomain = "//cmscore.prod-v1.ye-dian.com";
 }
 
 let __API_ROOT = {
-	// user
+	// 登录/注册 发短信验证码
 	sendSMS: userApiDomain + "/auth/sms?_type=register",
+	// 短信登录
 	smsLogin: userApiDomain + "/auth/user/mobile/smslogin",
+	// 微信授权登录
 	weChatAuth: userApiDomain + "/auth/wechat?",
+	// 获取用户信息
 	getUserInfo: userApiDomain + "/internal/userInfo?_type=User",
+	// 通过用户ID获取用户信息
 	getUserInfoById: userApiDomain + "/internal/users/",
+	// 注册微信SDK
 	getWeChatSDKSign: userApiDomain + "/auth/wechat/signature",
-
+	// 获取社区banner列表
 	getCommunityBanner: cmsCoreApiDomain + "/public/graphql?",
+	// 通过bannerID获取banner详情
 	getBannerById: cmsCoreApiDomain + "/public/graphql?",
-
+	// 
 	getIndexUserList: "/api/userlist",
-
+	// 获取标签列表
 	getTags: commontCoreApiDomain + "/community/tags",
+	// 创建标签
 	createTag: commontCoreApiDomain + "/community/tag",
-
+	// 获取商家详情
 	getVenues: venuesCoreApiDomain + "/public/graphql?",
-
+	// 获取po文列表
 	getMessage: commontCoreApiDomain + "/community/posts",
+	// 获取po文详情
 	getMessageInfo: commontCoreApiDomain + "/community/post",
-
+	// 发布新po文
 	postMessage: commontCoreApiDomain + "/community/post",
-
+	// 上传文件(图片)
 	// uploadFile: "http://10.85.108.33:3007/public/file/upload",
 	uploadFile: venuesCoreApiDomain + "/public/file/upload",
 
+	/********************************************************************************** */
 	/**
 	 * Feedback 三要素 like favorite comment
 	 */
@@ -66,11 +75,10 @@ let __API_ROOT = {
 	getFollwers: commontCoreApiDomain + "/users/follows",
 	creatFollow: commontCoreApiDomain + "/users/follows",
 	deleteFollow: commontCoreApiDomain + "/users/follows",
+	/********************************************************************************** */
 
 	deletePost: commontCoreApiDomain + "/community/post",
 	infrom: commontCoreApiDomain + "/users/comment/inform",
-
-
 	commentMessage: commontCoreApiDomain + "/users/comments",
 	getComments: commontCoreApiDomain + "/users/comments/byTargetId",
 	deleteComment: commontCoreApiDomain + "/users/comment",
